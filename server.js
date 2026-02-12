3fdbdaae2f7e0bb50d1d0ac326d9bc0fa6738ce3
krishnalasya <krishna@edgeforce.in>
2026-02-12 11:43:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,22 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [' http://localhost:5173/'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 require("./utils/reminderJob");
 app.use('/api/auth', require('./routes/authRoutes'));
