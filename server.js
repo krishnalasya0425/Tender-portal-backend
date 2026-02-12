8db92a37c19b1ca10f72201700451162a4f2acb9
krishnalasya <krishna@edgeforce.in>
2026-02-12 11:47:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-const allowedOrigins = [' http://localhost:5173/'];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://palegoldenrod-ostrich-750270.hostingersite.com"
+];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true
 }));
 
-app.use(express.json());
-require("./utils/reminderJob");
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/tenders', require('./routes/tenderRoutes'));
+app.options("*", cors());
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+require("./utils/reminderJob");
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tenders", require("./routes/tenderRoutes"));
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
