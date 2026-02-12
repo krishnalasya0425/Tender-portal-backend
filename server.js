bcf4b286fd8e0482728bcdc43e930cf376d31324
krishnalasya <krishna@edgeforce.in>
2026-02-12 11:50:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,26 +8,20 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://palegoldenrod-ostrich-750270.hostingersite.com"
-];
-
 app.use(cors({
-  origin: allowedOrigins,
+  origin: true,
   credentials: true
 }));
 
-app.options("*", cors());
-
 app.use(express.json());
-
-require("./utils/reminderJob");
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tenders", require("./routes/tenderRoutes"));
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 
